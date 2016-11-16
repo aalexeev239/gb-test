ac8255081d9134202e35208c318d0f58f07f18ec
Andrey Alexeev <alexeev.andrey.a@gmail.com>
2016-11-16 16:35:23 +0300

    fix

--- client/components/Intro/index.js
+++ client/components/Intro/index.js
@@ -6,7 +6,7 @@ export default ({handleClick, isDisabled}) => (
   <div>
     <h2 className={style.content__title}>Начать тест</h2>
 
-    <p className={style.content__text}>Спойлер: правильными являются все первые варианты ответов2</p>
+    <p className={style.content__text}>Спойлер: правильными являются все первые варианты ответов</p>
 
     <button className={style.btn} onClick={handleClick} disabled={isDisabled}>Начать</button>
   </div>
